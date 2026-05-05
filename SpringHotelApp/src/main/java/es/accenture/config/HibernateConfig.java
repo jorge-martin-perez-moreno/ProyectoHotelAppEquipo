c36f31d5ab2c607c933abe2586e860620b170487
Javier Roldán Pomareta <javierroldanpomareta@hotmail.com>
2026-05-05 13:33:24 +0200

    arreglo setup

---
+++ SpringHotelApp/src/main/java/es/accenture/config/HibernateConfig.java
@@ -0,0 +1,59 @@
+package es.accenture.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration //Anotación de configuración de Spring, la lee al arrancar y define los beans
+@EnableTransactionManagement //Anotación que activa las transaciones
+@PropertySource("classpath:application.properties") // Anotación para cargar el properties
+public class HibernateConfig {
+
+    @Autowired //Anotación para la injection de dependencias para leer el application properties
+    private Environment env;
+
+    @Bean // Bean para crear la conexión con BBDD y mapea las entidades
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(env.getProperty("db.driver"));
+        ds.setUrl(env.getProperty("db.url"));
+        ds.setUsername(env.getProperty("db.username"));
+        ds.setPassword(env.getProperty("db.password"));
+        return ds;
+    }
+
+    @Bean // Conecta con BBDD
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setPackagesToScan("es.accenture.model");
+
+        Properties props = new Properties();
+        props.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        props.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+
+        factory.setHibernateProperties(props);
+
+        return factory;
+    }
+
+    @Bean // Hace las transacciones
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager tx = new HibernateTransactionManager();
+        tx.setSessionFactory(sessionFactory);
+        return tx;
+    }
+}
---
+++ SpringHotelApp/src/main/java/es/accenture/config/WebConfig.java
@@ -0,0 +1,22 @@
+package es.accenture.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration //Anotación de configuración de Spring, la lee al arrancar y define los beans
+@EnableWebMvc //Anotación que activa los controladores
+@ComponentScan(basePackages = "es.accenture.controller") //Anotación que escanea el paquete y busca los beans
+public class WebConfig {
+
+    @Bean
+    public ViewResolver viewResolver() { //Convierte lo que le devuelve el controller en una jsp
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setPrefix("/WEB-INF/views/");
+        vr.setSuffix(".jsp");
+        return vr;
+    }
+}
