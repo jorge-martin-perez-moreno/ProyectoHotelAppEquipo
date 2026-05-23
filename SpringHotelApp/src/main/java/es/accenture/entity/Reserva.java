67476598da64b3330f650768968a196e30ef8f8d
Javier Roldán Pomareta <javierroldanpomareta@hotmail.com>
2026-05-23 11:43:36 +0200

    tipos de entity reservas vueltos a poner como estaban

--- SpringHotelApp/src/main/java/es/accenture/entity/Reserva.java
+++ SpringHotelApp/src/main/java/es/accenture/entity/Reserva.java
@@ -1,6 +1,8 @@
 package es.accenture.entity;                          //esto lo hace cualquiera entero A o B
 
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -13,62 +15,88 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.springframework.format.annotation.DateTimeFormat;
 
-
+/**
+ * Clase entidad que representa una reserva registrada en el hotel.
+ * 
+ * Esta clase almacena la informacion de las reservas,
+ * incluyendo el huesped, la habitacion, las fechas de estancia,
+ * el tipo de pension y el estado de la reserva.
+ * 
+ * @author danih y javi
+ * @version 1.0
+ */
 @Entity // Anotación para representar una tabla en BBDD
 @Table(name="reservas") // Anotación que indica cómo se llama la tabla que representa
 public class Reserva {
 	
-	public enum EstadoReserva {PENDIENTE,CONFIRMADA,CANCELADA} //clase interna para crear los enum y poner las opciones que se quiere establecer
-	public enum TipoPension {ALOJAMIENTO,MEDIA,COMPLETA} //clase interna para crear los enum y poner las opciones que se quiere establecer
+	/**
+	 * Enumeracion que define los posibles estados de una reserva. ENUM
+	 */
+	public enum EstadoReserva {PENDIENTE,CONFIRMADA,CANCELADA}
+	/**
+	 * Enumeracion que define los distintos tipos de pension disponibles. ENUM
+	 */
+	public enum TipoPension {ALOJAMIENTO,MEDIA,COMPLETA}
 	
+	/*
+	 * Identificador unico de la reserva.
+	 */
 	@Id //clave primaria INT
-	@GeneratedValue(strategy=GenerationType.IDENTITY) //GenerateValue dice a BBDD que genere ID automático cada vez que guardemos una reserva con autoincrement que es IDENTITY INT
-	@Column(name="id_reserva",nullable=false) //Anotación para decir a Spring que la columna es id_reserva y no puede estar vacía ni repetirse INT
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_reserva",nullable=false)
 	private int idReserva;
 	
-	@ManyToOne(fetch=FetchType.EAGER) //Anotación para la relación N1 con huespec, muchas reservas pueden ser de un solo huesped, Lazy porque se dan datos bajo demanda y sino sale por defecto EAGER por ser ManyToOne
-	@JoinColumn(name="id_huesped",nullable=false) //Anotación para decir a Spring a que tabla referencia y que es requerido INT
+	/*
+	 * Huesped de la reserva.
+	 */
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="id_huesped",nullable=false)
 	private Huesped huesped;
 	
-	@ManyToOne(fetch=FetchType.EAGER) //Anotación para la relación N1 con habitación, muchas reservas pueden estar asociadas a una sola habitación, Lazy porque se dan datos bajo demanda y sino sale por defecto EAGER por ser ManyToOne
-	@JoinColumn(name="id_habitacion",nullable=false) //Anotación para decir a Spring a que tabla referencia y que es requerido INT
+	/*
+	 * Habitacion de la reserva.
+	 */
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="id_habitacion",nullable=false)
 	private Habitacion habitacion;
 	
-//	@DateTimeFormat(pattern="yyyy-MM-dd") //Anotación para el formato de la fecha porque sino spring no la reconoce como fecha
-	@Column(name="fecha_entrada",nullable=true) //Anotación para decir a Spring que la columna es fecha_entrada y no puede estar vacía ni repetirse DATE
-	private java.sql.Date fechaEntrada;
+	/*
+	 * Fecha de entrada de la reserva.
+	 */
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@Column(name="fecha_entrada",nullable=true)
+	private Date fechaEntrada;
 	
-//	@DateTimeFormat(pattern="yyyy-MM-dd") //Anotación para el formato de la fecha porque sino spring no la reconoce como fecha
-	@Column(name="fecha_salida",nullable=true) //Anotación para decir a Spring que la columna es fecha_salida y no puede estar vacía ni repetirse DATE
-	private java.sql.Date fechaSalida;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@Column(name="fecha_salida",nullable=true)
+	private Date fechaSalida;
 	
 	@Enumerated(EnumType.STRING)
-	@Column(name="tipo_pension") //Anotación para decir a Spring que la columna es tipo_pension ENUM
+	@Column(name="tipo_pension")
 	private TipoPension tipoPension;
 	
-	@Enumerated(EnumType.STRING) //Anotación para los enum, le dice a BBDD que son strings
-	@Column(name="estado_reserva") //Anotación para decir a Spring que la columna es estado_reserva ENUM
+	@Enumerated(EnumType.STRING)
+	@Column(name="estado_reserva")
 	private EstadoReserva estadoReserva;
 	
-	@Column(name="numero_huespedes",nullable=false) //Anotación para decir a Spring que la columna es numero_huespedes y no puede repetirse INT
+	@Column(name="numero_huespedes",nullable=false)
 	private int numeroHuespedes;
 	
-	@Column(name="observaciones")  //Anotación para decir a Spring que la columna es observaciones TEXT
-	private String observaciones; //aunque coincida el nombre y no haga falta Israel dice en el video que la buena práctica es ponerlo
+	@Column(name="observaciones") 
+	private String observaciones;
 	
 	public Reserva() {
 		
 	}
 	
 	public Reserva(Huesped huesped,Habitacion habitacion,java.sql.Date fechaEntrada,java.sql.Date fechaSalida,TipoPension tipoPension,EstadoReserva estadoReserva,int numeroHuespedes,String observaciones) {
-		
-		//se quita id_reserva porque es autoincremental
-		
+				
 		this.huesped=huesped;
 		this.habitacion=habitacion;
-		this.fechaEntrada=(java.sql.Date) fechaEntrada;
-		this.fechaSalida=(java.sql.Date) fechaSalida;
+		this.fechaEntrada=fechaEntrada;
+		this.fechaSalida=fechaSalida;
 		this.tipoPension=tipoPension;
 		this.estadoReserva=estadoReserva;
 		this.numeroHuespedes=numeroHuespedes;
@@ -79,7 +107,7 @@ public class Reserva {
 		return idReserva;
 	}
 
-	public void setIdReserva(int idReserva) { //se quita el setter porque es autoincremental y se pone solo
+	public void setIdReserva(int idReserva) {
 		this.idReserva = idReserva;
 	}
 
@@ -87,7 +115,7 @@ public class Reserva {
 		return huesped;
 	}
 
-	public void setHuesped(Huesped huesped) { //este establece la relacion con Huesped
+	public void setHuesped(Huesped huesped) {
 		this.huesped = huesped;
 	}
 	
@@ -99,20 +127,20 @@ public class Reserva {
 		this.habitacion = habitacion;
 	}
 
-	public java.sql.Date getFechaEntrada() {
+	public Date getFechaEntrada() {
 		return fechaEntrada;
 	}
 
-	public void setFechaEntrada(java.sql.Date fechaEntrada) {
-		this.fechaEntrada = (java.sql.Date) fechaEntrada;
+	public void setFechaEntrada(Date fechaEntrada) {
+		this.fechaEntrada = fechaEntrada;
 	}
 
-	public java.sql.Date getFechaSalida() {
+	public Date getFechaSalida() {
 		return fechaSalida;
 	}
 
-	public void setFechaSalida(java.sql.Date fechaSalida) {
-		this.fechaSalida = (java.sql.Date) fechaSalida;
+	public void setFechaSalida(Date fechaSalida) {
+		this.fechaSalida = fechaSalida;
 	}
 
 	public TipoPension getTipoPension() {
